2a52966ba5fd0340ab41f6af50992ff21d2cd21e
Vluts69 <vidrikluts@gmail.com>
2026-04-02 16:05:56 +0300

    ter

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-async function game() {
-  const response = await fetch('https://tinkr.tech/sdb/Vidrik%20Luts/Databaas');
-  const data = await players.json();
-  console.log();
+const url = 'https://tinkr.tech/sdb/Vidrik%20Luts/Databaas';
+
+
+
+const game =async()=>{
+
+  let response = await fetch(url);
+  //console.log(response);
+
+  let data1 = await response.json();
+  console.log(data1);
+ 
+
+
+let players1 = data1.players;
+for (let player of players1) {
+ console.log(player);
+
+const newDiv = document.createElement('div');
+newDiv.classList.add('players');
+document.body.appendChild(newDiv);
+newDiv.style.position="absolute";
+newDiv.style.left= player.x + "px";
+newDiv.style.top= player.y + "py";
+
+
+
+const img = document.createElement('img');
+img.src = player.image;
+document.body.appendChild(img);
+console.log(img);
+}
 }
 game();
 
 
-//const players = [await players.json()];
-//for (const player of players) {
- // console.log(player);
+       
+        
 
-//const newDiv = document.createElement('div');
-//newDiv.classList.add('my-class');
-//document.body.appendChild(newDiv);
+
+

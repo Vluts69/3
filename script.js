2978c192309aa8a86138010b6a8c175d6e3f67fe
Vluts69 <vidrikluts@gmail.com>
2026-04-02 12:21:02 +0300

    njj

---
+++ script.js
@@ -0,0 +1,15 @@
+async function game() {
+  const response = await fetch('https://tinkr.tech/sdb/Vidrik%20Luts/Databaas');
+  const data = await players.json();
+  console.log();
+}
+game();
+
+
+//const players = [await players.json()];
+//for (const player of players) {
+ // console.log(player);
+
+//const newDiv = document.createElement('div');
+//newDiv.classList.add('my-class');
+//document.body.appendChild(newDiv);

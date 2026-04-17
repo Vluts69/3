43b2185b0f37760191bf015e77d106da2e7d2030
Vidrik Luts <vidrik.luts@tptlive.ee>
2026-04-17 11:31:58 +0300

    terror

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const url = 'https://tinkr.tech/sdb/Vidrik%20Luts/Databaas';
-const pilt1 = document.querySelector  (".pilt");
-const players2 = document.querySelector("#players");
+const players2 = document.querySelector(".world");
+const pilt2 = "https://tinkr.tech/sdb_apps/wanderworld/images/player.png";
 const game =async()=>{
 
   let response = await fetch(url);
@@ -8,39 +8,51 @@ const game =async()=>{
 
   let data1 = await response.json();
   //console.log(data1);
- 
+  
+let time = setInterval(game, 2000);
+ clearInterval(time);
+ time.innerHTML = "";
 
 
 let players1 = data1.players;
 for (let player of players1) {
+
  //console.log(player);
 
 const newDiv = document.createElement('div');
 newDiv.classList.add('player');
 players2.appendChild(newDiv);
+
+
 newDiv.style.position="absolute";
 newDiv.style.left= player.x + "px";
 newDiv.style.top= player.y + "py";
 
+newDiv.appendChild(css);
+
 const messg = document.createElement("div");
 messg.id = "pilt3";
 const msg = player.message;
-pilt1.appendChild(messg, msg);
 messg.textContent = msg;
-console.log(msg);
+newDiv.appendChild(messg, msg);
+//console.log(msg);
 
 
-
+const nimi = document.createElement("div");
+nimi.id = "nimii";
+const nimi2 = player.username;
+nimi.textContent = nimi2;
+newDiv.appendChild(nimi);
 
 const img = document.createElement('img');
 img.src = "https://tinkr.tech/" + player.image;
-pilt1.appendChild(img);
+newDiv.appendChild(img);
 //console.log(player);
 
 
+}
+}
 
-}
-}
 game();
 
 
